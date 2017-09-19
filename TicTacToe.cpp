7698fdf438f11a89464ecd3fd3d80b2f3a3c7f4e
EthanReese <ethan.reese01@gmail.com>
2017-09-19 13:05:06 -0700

    Created some initial variables

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,3 +3,29 @@
  *Date: September 19, 2017
  *Created by: Ethan Reese
  */
+#include <iostream>
+#include <cstdlib>
+//Create constants that can show turns and possession of squares
+#define BLANK = 0;
+#define X_HELD = 1;
+#define O_HELD = 2;
+#define X_TURN = 1;
+#define O_TURN = 2;
+
+
+int main(){
+	//Make counters that can count how many wins each player has
+	int xWins = 0;
+	int oWins = 0;
+	int tie = 0;
+	
+	//Make a variable that tracks if a user wants to keep playing
+	boolean stillPlaying = true;
+
+	while(stillPlaying){
+		//Make the initial turn X
+		int turn  = X_TURN;
+	}
+	return 0;
+}
+
