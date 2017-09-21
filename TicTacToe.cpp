2378aecf7e6665368e6bc620f96bf36b2e0ad312
EthanReese <ethan.reese01@gmail.com>
2017-09-21 09:26:39 -0700

    Made some changes

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -12,6 +12,9 @@
 #define X_TURN = 1;
 #define O_TURN = 2;
 
+//Define all the functions
+boolean checkWin(int player, int[][] board);
+boolean checkTie(int[][] board);
 
 int main(){
 	//Make counters that can count how many wins each player has
@@ -22,10 +25,51 @@ int main(){
 	//Make a variable that tracks if a user wants to keep playing
 	boolean stillPlaying = true;
 
+	//Make a two dimensional int array that represents the game board
+	int board[3][3];
+
 	while(stillPlaying){
 		//Make the initial turn X
 		int turn  = X_TURN;
+		//Main game loop
+		while(checkWin(X_HELD, board) == false && checkWin(O_HELD, board) && !checkTie()){
+
+		}
 	}
 	return 0;
 }
+
+
+//A function that checks if the board has a win condition
+boolean checkWin(int player, int[][] board){
+	//Check all of the possible win conditions
+	if(board[0][0] == player && board[0][1] == player && board[0][2] == player){
+		return true;
+	}
+	if(board[1][0] == player && board[1][1] == player && board[1][2] == player){
+		return true;
+	}
+	if(board[2][0] == player && board[2][1] == player && board[2][2] == player){
+		return true;
+	}
+	if(board[0][0] == player && board[1][0] == player && board[2][0] == player){
+		return true;
+	}
+	if(board[0][1] == player && board[1][1] == player && board[2][1] == player){
+		return true;
+	}
+	if(board[0][2] == player && board[1][2] == player && board[2][2] == player){
+		return true;
+	}
+	if(board[0][0] == player && board[1][1] == player && board[2][2]== player){
+		return true;
+	}
+	if(board[2][0] == player && board[1][1] == player &&board[0][2] == player){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
 
